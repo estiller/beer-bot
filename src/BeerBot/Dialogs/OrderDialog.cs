173b7fcd7ddd74bac9bab1ac9f283e38c34e00ac
Eran Stiller <git@stiller.blog>
2017-07-12 13:13:30 +0300

    Order beer

---
+++ src/BeerBot/Dialogs/OrderDialog.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Configuration;
+using System.Linq;
+using BeerBot.BeerApi.Client;
+using BeerBot.BeerApi.Client.Models;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.FormFlow;
+
+namespace BeerBot.Dialogs
+{
+    [Serializable]
+    public class BeerOrder
+    {
+        [Prompt("What beer would you like?")]
+        public string BeerName { get; set; }
+        [Prompt("Which chaser would you like next to your beer? {||}")]
+        public Chaser Chaser { get; set; }
+        [Prompt("How about something to eat? {||}")]
+        public SideDish Side { get; set; }
+    }
+
+    public enum Chaser
+    {
+        [Describe("Whiskey")]
+        Whiskey = 1,
+        [Describe("Vodka")]
+        Vodka,
+        [Describe("Liquor")]
+        Liquor,
+        [Describe("Water")]
+        Water
+    }
+
+    public enum SideDish
+    {
+        Fries = 1,
+        Pretzels,
+        Nachos
+    }
+
+    public static class OrderDialog
+    {
+        private static readonly IBeerAPI BeerApiClient = new BeerAPI(new Uri(ConfigurationManager.AppSettings["BeerApiUrl"]));
+
+        public static IDialog<BeerOrder> CreateDialog(string beerName = null)
+        {
+            return new FormDialog<BeerOrder>(new BeerOrder {BeerName = beerName }, BuildForm, FormOptions.PromptInStart);
+        }
+
+        private static IForm<BeerOrder> BuildForm()
+        {
+            return new FormBuilder<BeerOrder>()
+                .Field(nameof(BeerOrder.BeerName), validate: async (state, value) =>
+                {
+                    var beerName = (string) value;
+                    var possibleBeers = await BeerApiClient.BeersGetBySearchTermAsync(beerName);
+                    
+                    Beer exactMatch = possibleBeers.FirstOrDefault(b => b.Name.Equals(beerName, StringComparison.CurrentCultureIgnoreCase));
+                    if (exactMatch != null)
+                    {
+                        return new ValidateResult { IsValid = true, Value = possibleBeers[0].Name };
+                    }
+
+                    switch (possibleBeers.Count)
+                    {
+                        case 0:
+                            return new ValidateResult { IsValid = false, Feedback = "Don't know such beer... Try again." };
+                        case 1:
+                            return new ValidateResult {IsValid = true, Value = possibleBeers[0].Name};
+                    }
+                    return new ValidateResult
+                    {
+                        IsValid = false,
+                        Feedback = "I'm not sure which one", 
+                        Choices = possibleBeers.Select(b => new Choice
+                        {
+                            Value = b.Name,
+                            Description = new DescribeAttribute(b.Name),
+                            Terms = new TermsAttribute(b.Name)
+                        })
+                    };
+                })
+                .AddRemainingFields()
+                .Build();
+        }
+    }
+}
--- src/BeerBot/Dialogs/RootDialog.cs
+++ src/BeerBot/Dialogs/RootDialog.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Diagnostics;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using BeerBot.BeerApi.Client.Models;
@@ -10,6 +11,8 @@ namespace BeerBot.Dialogs
     [Serializable]
     public class RootDialog : IDialog
     {
+        private Beer _recommendedBeer;
+
         public Task StartAsync(IDialogContext context)
         {
             context.Wait(InitialMessageReceivedAsync);
@@ -40,43 +43,61 @@ namespace BeerBot.Dialogs
             var message = await argument;
             if (Regex.IsMatch(message.Text, ".*order.*", RegexOptions.IgnoreCase))
             {
-                await context.PostAsync("Ordering will be available soon!");
+                context.Call(OrderDialog.CreateDialog(), BeerOrderedAsync);
+                return;
             }
-            else if (Regex.IsMatch(message.Text, ".*recommend.*", RegexOptions.IgnoreCase))
+            if (Regex.IsMatch(message.Text, ".*recommend.*", RegexOptions.IgnoreCase))
             {
                 context.Call(RecommendationDialog.Dialog, BeerRecommendedAsync);
                 return;
             }
-            else if (Regex.IsMatch(message.Text, "^help.*", RegexOptions.IgnoreCase))
+            if (Regex.IsMatch(message.Text, "^help.*", RegexOptions.IgnoreCase))
             {
                 await context.PostAsync("You can type 'order' for ordering beers and 'recommend' for getting some recommendations");
+                context.Wait(MessageReceivedAsync);
             }
             else if (Regex.IsMatch(message.Text, "^(bye|adios).*", RegexOptions.IgnoreCase))
             {
                 await context.PostAsync("Thank you. Come again!");
                 context.Done(true);
-                return;
             }
             else
             {
                 await context.PostAsync("I don't quite understand. Please type 'help' for getting acquianted with me.");
+                context.Wait(MessageReceivedAsync);
             }
+        }
+
+        private async Task BeerOrderedAsync(IDialogContext context, IAwaitable<BeerOrder> result)
+        {
+            var beerOrder = await result;
+            await context.PostAsync($"Your order of {beerOrder.BeerName} and {beerOrder.Chaser} with {beerOrder.Side} is coming right up!");
+            await context.PostAsync("So what would you like to do next?");
             context.Wait(MessageReceivedAsync);
         }
 
         private async Task BeerRecommendedAsync(IDialogContext context, IAwaitable<Beer> argument)
         {
-            var beer = await argument;
-            PromptDialog.Confirm(context, async (ctx, res) =>
+            Debug.Assert(_recommendedBeer == null);
+            _recommendedBeer = await argument;
+
+            // Can't use anonymous method as anonymous method which capture environment artifacts are not serializable
+            PromptDialog.Confirm(context, CompleteBeerRecommendationAsync, $"Would you like to order '{_recommendedBeer.Name}'?");
+        }
+
+        private async Task CompleteBeerRecommendationAsync(IDialogContext context, IAwaitable<bool> shouldOrder)
+        {
+            Debug.Assert(_recommendedBeer != null);
+            if (await shouldOrder)
             {
-                bool shouldOrder = await res;
-                if (shouldOrder)
-                {
-                    await ctx.PostAsync("Ordering will be available soon!");
-                }
-                await ctx.PostAsync("So what would you like to do next?");
-                ctx.Wait(MessageReceivedAsync);
-            }, $"Would you like to order '{beer.Name}'?");
+                context.Call(OrderDialog.CreateDialog(_recommendedBeer.Name), BeerOrderedAsync);
+            }
+            else
+            {
+                await context.PostAsync("So what would you like to do next?");
+                context.Wait(MessageReceivedAsync);
+            }
+            _recommendedBeer = null;
         }
     }
 }
